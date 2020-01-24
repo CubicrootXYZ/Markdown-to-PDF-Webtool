78b89969a26c5db958b09c26ee18de841cf0b334
Cubicroot <24812442+CubicrootXYZ@users.noreply.github.com>
2020-01-24 14:01:43 +0100

    Update run_old.py

--- api/run_old.py
+++ api/run_old.py
@@ -5,11 +5,16 @@ from weasyprint import HTML
 
 head = """
         <style>
- 
+    @font-face {
+        font-family: 'Politics Head';
+        src: url(fonts/ph.ttf);
+    }
 
     body {
         font-family: 'Open Sans', sans-serif;
         font-weight: normal;
+        font-size: 85%;
+        overflow-wrap: break-word !important;
     }
     h1, h2, h3, h4, h5, h6 {
         color: #ff8800;
@@ -36,8 +41,8 @@ head = """
         font-size: 100%;
     }
     blockquote {
-        margin-left: 30px;
-        padding: 3px;
+        margin-left: 1rem;
+        padding: 1rem;
         background: #FFE1BF;
         border-left: 2px solid #ff8800;
         }
@@ -49,6 +54,35 @@ head = """
     a, a:active, a:hover, a:visited {
         color: #ff8800;
     }
+    code {
+        background: #e3e3e3;
+        padding: 2px;
+        border-radius: 4px;
+    }
+    p {
+        padding-bottom: 0.2rem;
+        margin-bottom: 0rem; 
+    }
+
+    @page {
+        size: A4;
+        margin: 18mm;
+    @bottom-center{
+        content: "Seite " counter(page) " / " counter(pages);
+        font-family: "Open Sans", sans-serif;
+        color: #969292;
+        font-size: 75%;
+    }
+    }
+    .end {
+        width: 100%;
+        text-align: center;
+        padding-top: 5rem;
+        color: #969292;
+        font-size: 75%;
+        display: block;
+    }
+
     
     </style>
     """
@@ -64,7 +98,8 @@ def makePdfFromHtml(self, html, title):
         'margin-bottom': '25mm',
         'margin-left': '25mm',
         'encoding': "UTF-8",
-
+        'footer-html': 'D:\github_repos\mdwntopdf\\api\\templates\\footer.html',
+        'header-html': 'D:\github_repos\mdwntopdf\\api\\templates\\header.html',
         'no-outline':None,
         'title': title
         }
@@ -134,10 +169,11 @@ class MarkdownToPdf():
         
 
         if md != False:
-            md = head+convertMarkdownToHtml(md)
+            md = head+convertMarkdownToHtml(md.replace("/n", "<br>"))
+            md += "<br><div class='end'>www.piratenpartei.de</div>"
             key = datetime.datetime.now().strftime("%Y-%m-%d_")+randomString()
-
-            file_ = HTML(string=md).write_pdf()
+            font_config = FontConfiguration()
+            file_ = HTML(string=md).write_pdf(font_config=font_config)
 
             
 
@@ -146,7 +182,7 @@ class MarkdownToPdf():
                     'file_key': key,
                     'file': file_
                 })
-                data = {"status": "success", "data": {"key": key, "url": baseurl+"/file?key="+key}}
+                data = {"status": "success", "data": {"key": key, "url": baseurl+key}}
             except Exception as e:
                 print(e)
                 data = {"status": "failure", "data": {}}
@@ -204,7 +240,6 @@ class GetFile():
 
         try: 
             resp.set_header("Content-Disposition", "attachment; filename="+key+".pdf")
-            resp.content_type = "application/pdf"
             resp.data = val['file']
             resp.status = falcon.HTTP_200
         except:
@@ -251,17 +286,17 @@ class CheckFile():
         try:
             val = db.table(prefix+'files').where('file_key', key).count()
         except: 
-            resp.status = falcon.HTTP_503
+            esp.status = falcon.HTTP_503
             return
 
         if val > 0:
             try: 
-                resp.body = json.dumps({"status": "success", "data": {"file_status": "exists", "url": baseurl+"/file?key="+key}}) 
+                resp.body = json.dumps({"status": "success", "data": {"file_status": "exists", "url": baseurl+"file?key="+key}}) 
                 resp.status = falcon.HTTP_200
             except:
                 resp.status = falcon.HTTP_404
         else: 
-            resp.body = json.dumps(val) 
+            resp.body = json.dumps({"status": "success", "data": {"file_status": "not exists"}}) 
 
         
 
@@ -270,7 +305,5 @@ api.req_options.auto_parse_form_urlencoded=True
 api.add_route('/markdowntopdf', MarkdownToPdf())
 api.add_route('/file', GetFile())
 api.add_route('/checkfile', CheckFile())
-
-
 
 
